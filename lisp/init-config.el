a9f6e8965c6f18c630bd224722a358600a753c63
pwzy <2386900869@qq.com>
2021-07-09 23:38:09 +0800

    my emacs config fix

---
+++ lisp/init-config.el
@@ -0,0 +1,77 @@
+;;;;;;;;;;;;;
+;; 包管理器 ;;
+;;;;;;;;;;;;;
+(require 'init-package)
+
+;;;;;;;;;;;;;;
+;; 基础配置  ;;
+;;;;;;;;;;;;;;
+(require 'init-basic)
+
+;;;;;;;;;;;;;
+;; 用户界面 ;;
+;;;;;;;;;;;;;
+(require 'init-ui)
+
+; ;;;;;;;;;;;;;
+; ;; 代码补全 ;;
+; ;;;;;;;;;;;;;
+; (require 'init-complete)
+; (require 'init-debug)
+;
+; ;;;;;;;;;;;;;;
+; ;; lsp-mode ;;
+; ;;;;;;;;;;;;;;
+; (require 'init-lsp)
+;
+; ;;;;;;;;;;;;;;
+; ;; treemacs ;;
+; ;;;;;;;;;;;;;;
+; (require 'init-treemacs)
+;
+; ;;;;;;;;;;;;;;
+; ;; hydra ;;
+; ;;;;;;;;;;;;;;
+; (require 'init-hydra)
+;
+;
+; ;;;;;;;;;;;;;;;;;
+; ;; awesome-tab ;;
+; ;;;;;;;;;;;;;;;;;
+; (require 'init-awesome-tab)
+;
+;
+; ;;;;;;;;;;;;;;
+; ;; org ;;;;;;;
+; ;;;;;;;;;;;;;;
+; (require 'init-org)
+;
+;
+; ;;;;;;;;;;;;;;
+; ;; keybind ;;;
+; ;;;;;;;;;;;;;;
+; (require 'init-keybind)
+;
+;
+; ;;;;;;;;;;;;;;
+; ;; snail ;;;;;
+; ;;;;;;;;;;;;;;
+; (require 'init-snail)
+;
+; ;;;;;;;;;;;;;;
+; ;; eaf   ;;;;;
+; ;;;;;;;;;;;;;;
+; (require 'init-eaf)
+;
+; ;;;;;;;;;;;;;;
+; ;; imenu-list;
+; ;;;;;;;;;;;;;;
+; (require 'init-imenu)
+;
+; ;;;;;;;;;;;;;;
+; ;; projectile;
+; ;;;;;;;;;;;;;;
+; (require 'init-projectile)
+;
+
+(provide 'init-config)
